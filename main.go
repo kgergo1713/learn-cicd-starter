1fd0ef8a1da528b91be37c4b236d311276a4255d
kgergo1713 <kgergo1713@gmail.com>
2024-08-16 13:44:24 +0200

    cd11

--- main.go
+++ main.go
@@ -27,10 +27,10 @@ type apiConfig struct {
 var staticFiles embed.FS
 
 func main() {
-	err := godotenv.Load("/.env")
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
+#	err := godotenv.Load("/.env")
+#	if err != nil {
+#		log.Fatalf("Error loading .env file: %v", err)
+#	}
 
 	port := os.Getenv("PORT")
 	if port == "" {
