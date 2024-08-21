74193e791189d7780912be20aa81322cc5d0c279
kgergo1713 <kgergo1713@gmail.com>
2024-08-21 14:24:57 +0200

    main.go 16

--- main.go
+++ main.go
@@ -1,109 +1,109 @@
 package main
 
 import (
- "database/sql"
- "embed"
- "fmt"
- "io"
- "log"
- "net/http"
- "net/url"
- "os"
- "strings"
- "time"
+	"database/sql"
+	"embed"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 
- "github.com/go-chi/chi"
- "github.com/bootdotdev/learn-cicd-starter/internal/database"
- _ "github.com/lib/pq" // Import the PostgreSQL driver
+	"github.com/go-chi/chi"
+	"github.com/bootdotdev/learn-cicd-starter/internal/database"
+	_ "github.com/lib/pq" // Import the PostgreSQL driver
 )
 
 type apiConfig struct {
- DB *database.Queries
+	DB *database.Queries
 }
 
 //go:embed static/*
 var staticFiles embed.FS
 
 func main() {
- port := os.Getenv("PORT")
- if port == "" {
-  port = "8080" // default to 8080 if PORT is not set
- }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // default to 8080 if PORT is not set
+	}
 
- apiCfg := apiConfig{}
+	apiCfg := apiConfig{}
 
- dbURL := os.Getenv("DATABASE_URL")
- fmt.Println("DATABASE_URL:", dbURL)
- if dbURL == "" {
-  log.Println("DATABASE_URL environment variable is not set")
-  log.Println("Running without CRUD endpoints")
- } else {
-  parsedURL, err := addParseTimeParam(dbURL)
-  if err != nil {
-   log.Fatal(err)
-  }
-  db, err := sql.Open("postgres", parsedURL)
-  if err != nil {
-   log.Fatal(err)
-  }
-  err = db.Ping()
-  if err != nil {
-   log.Fatalf("Could not connect to the database: %v", err)
-  }
-  dbQueries := database.New(db)
-  apiCfg.DB = dbQueries
-  log.Println("Connected to database!")
- }
+	dbURL := os.Getenv("DATABASE_URL")
+	fmt.Println("DATABASE_URL:", dbURL)
+	if dbURL == "" {
+		log.Println("DATABASE_URL environment variable is not set")
+		log.Println("Running without CRUD endpoints")
+	} else {
+		parsedURL, err := addParseTimeParam(dbURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		db, err := sql.Open("postgres", parsedURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = db.Ping()
+		if err != nil {
+			log.Fatalf("Could not connect to the database: %v", err)
+		}
+		dbQueries := database.New(db)
+		apiCfg.DB = dbQueries
+		log.Println("Connected to database!")
+	}
 
- router := chi.NewRouter()
+	router := chi.NewRouter()
 
- router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-  f, err := staticFiles.Open("static/index.html")
-  if err != nil {
-   http.Error(w, err.Error(), http.StatusInternalServerError)
-   return
-  }
-  defer f.Close()
-  if _, err := io.Copy(w, f); err != nil {
-   http.Error(w, err.Error(), http.StatusInternalServerError)
-  }
- })
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		f, err := staticFiles.Open("static/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+		if _, err := io.Copy(w, f); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
 
- v1Router := chi.NewRouter()
+	v1Router := chi.NewRouter()
 
- if apiCfg.DB != nil {
-  v1Router.Post("/users", apiCfg.handlerUsersCreate)
-  v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerUsersGet))
-  v1Router.Get("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesGet))
-  v1Router.Post("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesCreate))
- }
+	if apiCfg.DB != nil {
+		v1Router.Post("/users", apiCfg.handlerUsersCreate)
+		v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerUsersGet))
+		v1Router.Get("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesGet))
+		v1Router.Post("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesCreate))
+	}
 
- v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/healthz", handlerReadiness)
 
- router.Mount("/v1", v1Router)
- srv := &http.Server{
-  Addr:              ":" + port,
-  Handler:           router,
-  ReadHeaderTimeout: time.Second * 5, // use seconds or it will default to nanoseconds
- }
+	router.Mount("/v1", v1Router)
+	srv := &http.Server{
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: time.Second * 5, // use seconds or it will default to nanoseconds
+	}
 
- log.Printf("Serving on port: %s\n", port)
- log.Fatal(srv.ListenAndServe())
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
 
 func addParseTimeParam(input string) (string, error) {
- const dummyScheme = "http://"
- if !strings.Contains(input, dummyScheme) {
-  input = "http://" + input
- }
- u, err := url.Parse(input)
- if err != nil {
-  return "", err
- }
- q := u.Query()
- q.Add("parseTime", "true")
- u.RawQuery = q.Encode()
- returnUrl := u.String()
- returnUrl = strings.TrimPrefix(returnUrl, dummyScheme)
- return returnUrl, nil
+	const dummyScheme = "http://"
+	if !strings.Contains(input, dummyScheme) {
+		input = "http://" + input
+	}
+	u, err := url.Parse(input)
+	if err != nil {
+		return "", err
+	}
+	q := u.Query()
+	q.Add("parseTime", "true")
+	u.RawQuery = q.Encode()
+	returnUrl := u.String()
+	returnUrl = strings.TrimPrefix(returnUrl, dummyScheme)
+	return returnUrl, nil
 }
